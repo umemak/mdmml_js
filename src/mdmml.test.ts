35590ac9cad8d99e6ccdd0d26c9ddf14371f2d2f
umemak <umematsu@gmail.com>
2022-05-03 17:10:39 +0900

    wip

--- src/mdmml.test.ts
+++ src/mdmml.test.ts
@@ -1,9 +1,76 @@
-import { atoi } from "./mdmml";
+import * as mdmml from "./mdmml"
 
-test('atoi: number', function () {
-    expect(atoi("1", 2)).toBe(1);
-});
+test('atoi: number', () => {
+    expect(mdmml.atoi("1", 2)).toBe(1)
+})
 
-test('atoi: not number', function () {
-    expect(atoi("a", 2)).toBe(2);
-});
+test('atoi: not number', () => {
+    expect(mdmml.atoi("a", 2)).toBe(2)
+})
+
+test('atoi: not number mix', () => {
+    expect(mdmml.atoi("1a", 2)).toBe(2)
+})
+
+test('Uint8ArrayJoin: normal', () => {
+    const src1 = new Uint8Array([1, 2, 3])
+    const src2 = new Uint8Array([4, 5, 6])
+    const want = new Uint8Array([1, 2, 3, 4, 5, 6])
+    expect(mdmml.Uint8ArrayJoin([src1, src2])).toStrictEqual(want)
+})
+
+test('itofb: i<256 f1', () => {
+    expect(mdmml.itofb(255, 1)).toStrictEqual(new Uint8Array([0xff]))
+})
+
+test('itofb: i<256 f2', () => {
+    expect(mdmml.itofb(255, 2)).toStrictEqual(new Uint8Array([0x00, 0xff]))
+})
+
+test('itofb: i<65536 f1', () => {
+    expect(mdmml.itofb(65535, 1)).toStrictEqual(new Uint8Array([0xff, 0xff]))
+})
+
+test('itofb: i<65536 f3', () => {
+    expect(mdmml.itofb(65535, 3)).toStrictEqual(new Uint8Array([0x00, 0xff, 0xff]))
+})
+
+test('itofb: i<16777216 f1', () => {
+    expect(mdmml.itofb(16777215, 1)).toStrictEqual(new Uint8Array([0xff, 0xff, 0xff]))
+})
+
+test('itofb: i<16777216 f4', () => {
+    expect(mdmml.itofb(16777215, 4)).toStrictEqual(new Uint8Array([0x00, 0xff, 0xff, 0xff]))
+})
+
+test('itofb: i<4294967296 f1', () => {
+    expect(mdmml.itofb(4294967295, 1)).toStrictEqual(new Uint8Array([0xff, 0xff, 0xff, 0xff]))
+})
+
+test('itofb: i<4294967296 f5', () => {
+    expect(mdmml.itofb(4294967295, 5)).toStrictEqual(new Uint8Array([0x00, 0xff, 0xff, 0xff, 0xff]))
+})
+
+test('itofb: i<=4294967296 f1', () => {
+    expect(mdmml.itofb(4294967296, 1)).toStrictEqual(new Uint8Array())
+})
+
+test('buildTitle: normal', () => {
+    expect(mdmml.buildTitle('abc')).toStrictEqual(new Uint8Array([0x00, 0xff, 0x03, 0x03, 0x61, 0x62, 0x63]))
+})
+
+test('buildTitle: empty', () => {
+    expect(mdmml.buildTitle('')).toStrictEqual(new Uint8Array([0x00, 0xff, 0x03, 0x00]))
+})
+
+test('buildTempo: 120',()=>{
+    expect(mdmml.buildTempo(120)).toStrictEqual(new Uint8Array([0x00, 0xff, 0x51, 0x03, 0x07, 0xa1, 0x20]))
+})
+
+test('tempoMs: bpm120',()=>{
+    expect(mdmml.tempoMs(120)).toBe(500000)
+})
+
+test('tempoMs: bpm140',()=>{
+    expect(mdmml.tempoMs(140)).toBe(428571)
+})
--- src/mdmml.ts
+++ src/mdmml.ts
@@ -1,20 +1,34 @@
 
+const MThd = new Uint8Array([0x4D, 0x54, 0x68, 0x64])
+const MTrk = new Uint8Array([0x4D, 0x54, 0x72, 0x6B])
+const EOT = new Uint8Array([0x00, 0xFF, 0x2F, 0x00])
+
 class MDMML {
     divisions = 960;
     title = '';
     tempo = 120;
-    header = [];
-    Conductor = Track;
+    header: Uint8Array;
+    Conductor: Track;
     Tracks: Track[] = [];
+    constructor() {
+        this.Conductor = new Track('', [], new Uint8Array())
+        this.header = new Uint8Array()
+    }
 }
 
 class Track {
     name = '';
     mmls: string[] = [];
-    smf = [];
+    smf: ArrayBuffer;
+
+    constructor(name: string, mmls: string[], smf: Uint8Array) {
+        this.name = name
+        this.mmls = mmls
+        this.smf = smf
+    }
 }
 
-function MDtoSMF(md: string): Uint8Array {
+export function MDtoSMF(md: string): ArrayBuffer {
     return SMF(MMLtoSMF(MDtoMML(md)))
 }
 
@@ -77,10 +91,7 @@ function MDtoMML(md: string): MDMML {
                     }
                 }
                 if (!found) {
-                    let tk = new Track()
-                    tk.name = name
-                    tk.mmls = mmls
-                    mm.Tracks.push(tk)
+                    mm.Tracks.push(new Track(name, mmls, new Uint8Array()))
                 }
             }
         }
@@ -89,11 +100,35 @@ function MDtoMML(md: string): MDMML {
 }
 
 function MMLtoSMF(mm: MDMML): MDMML {
+    for (let i = 0; i < mm.Tracks.length; i++) {
+        const t = mm.Tracks[i]
+        const events = toEvents(expand(t.mmls.join("")), i, mm.divisions)
+        mm.Tracks[i].smf = buildSMF(t.name, events, i)
+    }
+    mm.header = Uint8ArrayJoin([
+        MThd,
+        new Uint8Array([0x00, 0x00, 0x00, 0x06]),   // Length
+        new Uint8Array([0x00, 0x01]),               // Format
+        itofb(mm.Tracks.length + 1, 2),             // Tracks
+        itofb(mm.divisions, 2)                      // Divisions
+    ])
+
+    const title = buildTitle(mm.title)
+    const tempo = buildTempo(mm.tempo)
+    const smf = Uint8ArrayJoin([
+        MTrk,
+        itofb(title.length + tempo.length + 12, 4),
+        title,
+        tempo,
+        new Uint8Array([0x00, 0xFF, 0x58, 0x04, 0x04, 0x02, 0x18, 0x08]),   // Rhythm 4/4
+        EOT
+    ])
+    mm.Conductor = new Track('Conductor', [], smf)
     return mm
 }
 
-function SMF(mm: MDMML): Uint8Array {
-    return new Uint8Array();
+function SMF(mm: MDMML): ArrayBuffer {
+    return new ArrayBuffer(0);
 }
 
 function atoi(a: string, def: number): number {
@@ -104,4 +139,99 @@ function atoi(a: string, def: number): number {
     return ret
 }
 
-export {MDtoSMF, atoi}
+function expand(mml: string): string {
+    return mml
+}
+
+function toEvents(mml: string, ch: number, div: number): ArrayBuffer {
+    return new ArrayBuffer(0);
+}
+
+function buildSMF(title: string, events: ArrayBuffer, ch: number): ArrayBuffer {
+    return new ArrayBuffer(0);
+}
+
+function Uint8ArrayJoin(src: Uint8Array[]): Uint8Array {
+    let len = 0
+    for (let i = 0; i < src.length; i++) {
+        len += src[i].length
+    }
+    let dest = new Uint8Array(len)
+    let pos = 0
+    for (let i = 0; i < src.length; i++) {
+        dest.set(new Uint8Array(src[i]), pos)
+        pos += src[i].length
+    }
+    return dest
+}
+
+// itofb は number を f 桁の固定長バイナリにして返す
+function itofb(i: number, f: number): Uint8Array {
+    if (i < 256) {
+        if (f < 1) {
+            f = 1
+        }
+        let ret = new Uint8Array(f)
+        ret.set(new Uint8Array([i]), f - 1)
+        return ret
+    }
+    if (i < 256 * 256) {
+        if (f < 2) {
+            f = 2
+        }
+        let ret = new Uint8Array(f)
+        ret.set(new Uint8Array([i >> 8]), f - 2)
+        ret.set(new Uint8Array([i & 0xff]), f - 1)
+        return ret
+    }
+    if (i < 256 * 256 * 256) {
+        if (f < 3) {
+            f = 3
+        }
+        let ret = new Uint8Array(f)
+        ret.set(new Uint8Array([i >> 16]), f - 3)
+        ret.set(new Uint8Array([i >> 8]), f - 2)
+        ret.set(new Uint8Array([i & 0xff]), f - 1)
+        return ret
+    }
+    if (i < 256 * 256 * 256 * 256) {
+        if (f < 4) {
+            f = 4
+        }
+        let ret = new Uint8Array(f)
+        ret.set(new Uint8Array([i >> 24]), f - 4)
+        ret.set(new Uint8Array([i >> 16]), f - 3)
+        ret.set(new Uint8Array([i >> 8]), f - 2)
+        ret.set(new Uint8Array([i & 0xff]), f - 1)
+        return ret
+    }
+    return new Uint8Array()
+}
+
+function buildTitle(title: string): Uint8Array {
+    return Uint8ArrayJoin([
+        new Uint8Array([0x00, 0xff, 0x03]),
+        itofb(title.length, 1),
+        new TextEncoder().encode(title)
+    ])
+}
+
+function buildTempo(tempo: number): Uint8Array {
+    return Uint8ArrayJoin([
+        new Uint8Array([0x00, 0xff, 0x51, 0x03]),
+        itofb(tempoMs(tempo), 3)
+    ])
+}
+
+function tempoMs(t: number): number {
+    if (t == 0) {
+        return 0
+    }
+    return Math.trunc(60 * 1000 * 1000 / t)
+}
+
+// for Test
+export {
+    MDtoMML, MMLtoSMF, SMF, atoi, expand, toEvents, buildSMF, Uint8ArrayJoin,
+    itofb, buildTitle, buildTempo, tempoMs
+}
