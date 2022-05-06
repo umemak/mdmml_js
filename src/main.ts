809e2807a79e3bbd7869ade9c58d52fd3270b043
umemak <umematsu@gmail.com>
2022-05-06 22:20:09 +0900

    html

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { MDtoSMF } from './mdmml';
 
-export { MDtoSMF };
-
-console.log(Buffer.from(MDtoSMF("cdefg")).toString("binary"));
+interface Window { MDtoSMF(md: string): ArrayBuffer; }
+declare var window: Window;
+window.MDtoSMF = (md: string) => {
+    return MDtoSMF(md);
+};
