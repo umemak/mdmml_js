c4dedc13c1f67666c2218d69d11ca9a493758aa5
umemak <umematsu@gmail.com>
2022-05-31 19:10:32 +0900

    update html

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,12 @@
+const path = require('path');
+
 module.exports = {
   mode: 'development',
   entry: './src/main.ts',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, './public'), // 追加
+  },
   module: {
     rules: [
       {
